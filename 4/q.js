38ff115502d700f1685a2011611cc4b41fb2bf94
YuShuXiao <949516815@qq.com>
2023-02-23 10:50:51 +0800

    1

--- 4/q.js
+++ 4/q.js
@@ -3,7 +3,7 @@
  * @Author: YuShu Xiao
  * @Date: 2023-02-22 15:09:19
  * @LastEditors: YuShu Xiao
- * @LastEditTime: 2023-02-22 17:55:41
+ * @LastEditTime: 2023-02-23 10:47:42
  */
 const arr = [
   { uid: 56, name: "小a" },
@@ -22,25 +22,28 @@ const arr = [
 // }
 // console.log(qc(arr,"uid"));
 
-// // map2
-// function qc(arr, id) {
-//   const map = new Map();
-//   return arr.filter(item => !map.has(item[id]) && map.set(item[id],1));
-// }
-// console.log(qc(arr,"uid"));
-
-// reduce
-/**
- * @description:去重函数
- * @param {array} arr 去重的数组
- * @param {string} id    指定去重的字段
- * @return {array}
- */
+// map2
 function qc(arr, id) {
-  let hash = {};
-  return arr.reduce((arr1, item) => {
-    hash[item[id]] ? "": (hash[item[id]] = 1) && arr1.push(item)
-    return arr1
-  },[]);
+  // const map = new Map();
+  let hash={}
+  // return arr.filter(item => !map.has(item[id]) && map.set(item[id],1));
+  return arr.filter(item=> !hash[item[id]] && (hash[item[id]]=1)
+  )
 }
 console.log(qc(arr,"uid"));
+
+// // reduce
+// /**
+//  * @description:去重函数
+//  * @param {array} arr 去重的数组
+//  * @param {string} id    指定去重的字段
+//  * @return {array}
+//  */
+// function qc(arr, id) {
+//   let hash = {};
+//   return arr.reduce((arr1, item) => {
+//     hash[item[id]] ? "": (hash[item[id]] = 1) && arr1.push(item)
+//     return arr1
+//   },[]);
+// }
+// console.log(qc(arr,"uid"));
